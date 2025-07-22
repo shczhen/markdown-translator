36d3d7077784ffb364fe0aceb642d67fdef062a2
Suhaha <jklopsdfw@gmail.com>
2025-07-22 14:54:51 +0800

    chore: update LANGLINK_APP_ID for LangLink configuration

--- src/aiTranslatorZH.js
+++ src/aiTranslatorZH.js
@@ -4,7 +4,7 @@ import { writeFileSync } from "./lib.js";
 import { executeLangLinkTranslator } from "./langlinkClient.js";
 
 // LangLink 配置
-const LANGLINK_APP_ID = "b9084183-a95f-4645-97bc-55ce8da9b2ff";
+const LANGLINK_APP_ID = "2c3cf5a4-9f4f-4ab1-aa3e-37c5da35a94f";
 
 // Token 限制配置
 const OUTPUT_TOKEN_LIMIT = 30000;
